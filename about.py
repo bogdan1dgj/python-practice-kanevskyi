bfc40efc199e13651dddf4e5e51d6dc090063d00
bohdan <bogdan.kanevskiy23@gmail.com>
2026-09-09 12:00:54 +0300

    Add personal information script

---
+++ about.py
@@ -0,0 +1,12 @@
+def main():
+    name = "Bohdan"
+    surname = "Kanevskyi"
+    group = "IT-32"
+    birth_year = 2009
+
+    print(f"Name: {name} {surname}")
+    print(f"Group: {group}")
+    print(f"Age in 2026: {2026 - birth_year}")
+    print("Favourite language: Python")
+
+main()
